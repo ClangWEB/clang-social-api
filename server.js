2c46c4a1ec3ee8197e552047b749c08280a68d26
Shubham Lal <lal.shubham027@gmail.com>
2022-12-18 21:21:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const { readdirSync } = require("fs");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 
-
-
+app.use(cors(corsOptions))
 
 // ROUTES
 readdirSync("./routes").map((r) => app.use("/", require('./routes/' + r)));

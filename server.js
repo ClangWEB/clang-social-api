f581dcce2910079e63a3308ca13feecc7056add4
Shubham Lal <lal.shubham027@gmail.com>
2022-12-21 11:24:50 +0530

    Removed mail.html file

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();
 const { readdirSync } = require("fs");
 
 const app = express();
+dotenv.config();
 app.use(express.json());
 
 const corsOptions = {
@@ -20,11 +20,14 @@ app.use(cors(corsOptions))
 readdirSync("./routes").map((r) => app.use("/", require('./routes/' + r)));
 
 // DATABASE
+
+// mongoose.connect(process.env.DATABASE_URL, {
+//   useNewUrlParser: true,
+// })
+
 const connect = () => {
   mongoose.set('strictQuery', false);
-  mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-  })
+  mongoose.connect(process.env.DATABASE_URL)
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.log("Error trying to connect to DB", err))
 };

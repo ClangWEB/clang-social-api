84a5ea9370bf0e2f9b9809842a9bc8229c94d1ff
Shubham Lal <lal.shubham027@gmail.com>
2022-12-25 02:40:38 +0530

    All Posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,24 @@
 const Post = require("../models/Post");
 
+// CREATE POST
 exports.createPost = async (req, res) => {
     try {
         const post = await new Post(req.body).save();
         res.json(post);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// SHOW ALL POSTS
+exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post
+            .find()
+            .populate("user", "first_name last_name picture username gender")
+            .sort({ createdAt: "desc" });
+        res.json(posts);
     }
     catch (error) {
         return res.status(500).json({ message: error.message });

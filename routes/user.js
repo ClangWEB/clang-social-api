f5a7c97c799736765dbaf18e0c3364cd1c37076b
Shubham Lal <lal.shubham027@gmail.com>
2023-01-01 21:57:14 +0530

    Search User

--- routes/user.js
+++ routes/user.js
@@ -21,6 +21,8 @@ const {
     deleteRequest,
     search,
     addToSearchHistory,
+    getSearchHistory,
+    removeFromSearch,
 } = require("../controllers/user");
 const { authUser } = require("../middlewares/auth");
 
@@ -47,5 +49,7 @@ router.put("/unfriend/:id", authUser, unfriend);
 router.put("/deleteRequest/:id", authUser, deleteRequest);
 router.post("/search/:searchTerm", authUser, search);
 router.put("/addToSearchHistory", authUser, addToSearchHistory);
+router.get("/getSearchHistory", authUser, getSearchHistory);
+router.put("/removeFromSearch", authUser, removeFromSearch);
 
 module.exports = router;

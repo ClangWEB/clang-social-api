e15857f79b6372b9f3341d5218e08a35d0c00fe0
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-01-25 22:34:43 +0530

    Added headers

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Set-Cookie: cross-site-cookie=whatever; SameSite=None; Secure");
+  next();
+});
+
 const corsOptions = {
   origin: '*',
   credentials: true,
